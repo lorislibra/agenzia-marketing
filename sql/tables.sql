1dc135bafbc40adcd9b03ba6169ece29c325161e
loris <lorislibra21@gmail.com>
2022-04-06 20:24:10 +0200

    database tables

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,23 +1,25 @@
 DROP TABLE IF EXISTS 
-user_state, role, state, user, orders, reservation, product, cart 
+user_region, role, region, user, 
+item, product, sell_point,
+orders, reservation, cart 
 CASCADE;
 
 CREATE TABLE IF NOT EXISTS role(
+    id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS region(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE IF NOT EXISTS state(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    PRIMARY KEY(id)
-);
-
-CREATE TABLE IF NOT EXISTS user_state(
+CREATE TABLE IF NOT EXISTS user_region(
     user_id INT NOT NULL,
-    state_id INT NOT NULL,
-    PRIMARY KEY(user_id, state_id)
+    region_id INT NOT NULL,
+    PRIMARY KEY(user_id, region_id)
 );
 
 CREATE TABLE IF NOT EXISTS user(
@@ -30,28 +32,79 @@ CREATE TABLE IF NOT EXISTS user(
 
 CREATE TABLE IF NOT EXISTS reservation(
     id INT NOT NULL AUTO_INCREMENT,
-    userId INT NOT NULL,
+    user_id INT NOT NULL,
+    status INT NOT NULL,
+    sell_point_id INT NOT NULL,
+    cart_id INT NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS product(
+    sku VARCHAR(10) NOT NULL,
+    brand VARCHAR(30) NOT NULL,
+    category VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    price FLOAT NOT NULL,
+    PRIMARY KEY(sku)
+);
+
+CREATE TABLE IF NOT EXISTS item(
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
+    product_sku VARCHAR(10) NOT NULL,
+    quantity INT NOT NULL,
+    stock INT NOT NULL,
+    category VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS sell_point (
+    id INT NOT NULL AUTO_INCREMENT,
+    region_id INT NOT NULL,
+    address VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS cart_item (
+    cart_id INT NOT NULL, 
+    item_sku INT NOT NULL,
+    shipping_status INT NOT NULL,
+    PRIMARY KEY(cart_id, item_sku)
 );
 
 CREATE TABLE IF NOT EXISTS cart(
     id INT NOT NULL AUTO_INCREMENT,
-    userId INT NOT NULL,
+    user_id INT NOT NULL,
+    status INT NOT NULL,
     PRIMARY KEY(id)
 );
 
 ALTER TABLE user_state
-ADD CONSTRAINT FK_userId
+ADD CONSTRAINT FK_user_id
 FOREIGN KEY (user_id) REFERENCES user(id),
-ADD CONSTRAINT FK_userState
-FOREIGN KEY (state_id) REFERENCES state(id);
+ADD CONSTRAINT FK_user_region
+FOREIGN KEY (region_id) REFERENCES region(id);
+
+ALTER TABLE user
+ADD CONSTRAINT FK_role_id
+FOREIGN KEY (role_id) REFERENCES role(id);
+
+ALTER TABLE item
+ADD CONSTRAINT FK_product_sku
+FOREIGN KEY (product_sku) REFERENCES product(sku);
+
+ALTER TABLE sell_point
+ADD CONSTRAINT FK_sell_point_region
+FOREIGN KEY (region_id) REFERENCES region(id);
+
+/* DEFAULT VALUES */
+INSERT INTO role (id, name) VALUES 
+(0, "developer"),
+(1, "national administrator"),
+(2, "state group administrator"),
+(3, "state administrator");
 
 /* EXAMPLE ROWS */
 
-INSERT INTO user (email, password, role_id) VALUES ("test@gmail.com", "test", 1);
+/* DEV USER */
+INSERT INTO user (email, password, role_id)
+VALUES ("dev@dev.com", "dev", 0); /* change with hash of password */
