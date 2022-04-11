32c1b1a40627be6a4ab1d4e8282b8a0fd4faabea
loLLo32 <alessandro.lollato@barsanti.edu.it>
2022-04-11 10:52:50 +0200

    Inizio front

--- src/pages/items.php
+++ src/pages/items.php
@@ -1,7 +1,27 @@
 <?php
-
-require_once "src/middleware/session.php";
-require_once "src/repositories/manager.php";
-require_once "src/repositories/user.php";
-
+    require_once "src/middleware/session.php";
+    require_once "src/repositories/manager.php";
+    require_once "src/repositories/user.php";
+    require_once "src/template/lateral_menu.php";
 ?>
+
+<HTML>
+    <head>
+        <title>Showcase</title>
+
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
+    <body>
+
+    <?php
+        if(/*is_logged()*/true){
+            echo show_lateral_menu();
+        }
+        else{
+            header("login.php");
+        }
+    ?>
+
+    </body>
+</HTML>
---
+++ src/template/lateral_menu.php
@@ -0,0 +1,8 @@
+<?php
+    function show_lateral_menu(): string
+    {
+        $html_code = "";
+
+        return $html_code;
+    }
+?>
