5ca1e3906f96e48309a43b740b96f392b926ba83
loris <lorislibra21@gmail.com>
2022-04-14 18:51:27 +0200

    pages mode in deploy.php

--- scripts/deploy.php
+++ scripts/deploy.php
@@ -20,27 +20,41 @@ if (!$ftp || !ftp_login($ftp, $ftp_user, $ftp_passw)) {
 
 echo "Connected to $ftp_host, for user $ftp_user\n";
 
-$files = rglob("src/*", GLOB_NOSORT);
-array_push($files, ".env");
+ftp_mkdir($ftp, $ftp_project_path);
+
+$files = array();
+array_push($files, "src", ".env");
+
+if (count($argv) > 1 && $argv[1] == "pages") {
+    array_push($files, "src/pages");
+    array_push($files, ...rglob("src/pages/*", GLOB_NOSORT));
+} else {
+    array_push($files, ...rglob("src/*", GLOB_NOSORT));    
+}
+
 array_push($files, "src/pages/.user.ini");
 
-ftp_mkdir($ftp, $ftp_project_path);
-ftp_mkdir($ftp, "$ftp_project_path/src");
+function deploy(array $files)
+{
+    global $ftp_project_path, $ftp;
 
-foreach ($files as $filename) {
-    $remote_path = $ftp_project_path . "/" . $filename;
-    if (!is_dir($filename)) {
-        if (!$upload = ftp_put($ftp, $remote_path , $filename, FTP_BINARY)) {
-            echo ("FTP $filename upload has failed!\n");
+    foreach ($files as $filename) {
+        $remote_path = $ftp_project_path . "/" . $filename;
+        if (!is_dir($filename)) {
+            if (!$upload = ftp_put($ftp, $remote_path , $filename, FTP_BINARY)) {
+                echo ("FTP $filename upload has failed!\n");
+            } else {
+                echo("Uploaded $filename\n");
+            }
+        } else if (ftp_mkdir($ftp, $remote_path)) {
+            echo("Directory $remote_path created\n");
         } else {
-            echo("Uploaded $filename\n");
+            echo("Error creating directory $remote_path\n");
         }
-    } else if ($res = ftp_mkdir($ftp, $remote_path)) {
-        echo("Directory $remote_path created\n");
-    } else {
-        echo("Error creating directory $remote_path\n");
     }
 }
+
+deploy($files);
 
 // close the FTP connection
 ftp_close($ftp);
