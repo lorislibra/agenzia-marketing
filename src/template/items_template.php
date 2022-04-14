29910eba54ed1f3d270c2311e19352ba2e781a9b
loLLo32 <alessandro.lollato@barsanti.edu.it>
2022-04-14 18:52:02 +0200

    Item page without filters

--- src/template/items_template.php
+++ src/template/items_template.php
@@ -8,12 +8,6 @@ function show_items(array $items){
     if($items != null){
         foreach($items as $item){
             $html_code .= show_item($item);
-            $html_code .= show_item($item);
-            $html_code .= show_item($item);
-            $html_code .= show_item($item);
-            $html_code .= show_item($item);
-            $html_code .= show_item($item);
-            $html_code .= show_item($item);
         }
     }
 
@@ -25,7 +19,7 @@ function show_item(Item $item){
 
     $html_code = '
                 <div class="item_box">
-                    <img class="ib_image" alt="' . strtoupper($product->name). '" src="' . /*$product->image*/"https://peroni.it/wp-content/uploads/2019/09/peroni-33-feat.jpg" . '">
+                    <img class="ib_image" alt="' . strtoupper($product->name). '" src="' . $product->image . '">
                     <span class="ib_name">' . strtoupper($product->name) . '</span>
                 </div>
                 ';
