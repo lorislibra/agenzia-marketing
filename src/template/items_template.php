bb4159e4cba6aeadb5919df955001b3d0f603a36
loLLo32 <alessandro.lollato@barsanti.edu.it>
2022-04-14 17:13:37 +0200

    Item page changes

--- src/template/items_template.php
+++ src/template/items_template.php
@@ -1,20 +1,34 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once("src/repositories/item_repo.php");
 
 function show_items(){
-    global $connection;
-
+    $connection = DbManager::build_connection_from_env();
     $item_repo = new ItemRepo($connection);
     $items = $item_repo->get_all();
+    $html_code = '';
 
-    foreach($items as $item){
-        show_item($item->product->name);
+    if($items != null){
+        foreach($items as $item){
+            $html_code .= show_item($item);
+        }
     }
+
+    return $html_code;
 }
 
-function show_item(string $name){
-    echo $name;
+function show_item(Item $item){
+    $html_code = '
+                <div class="item_box">
+                    
+                </div>
+                ';
+    
+    return $html_code;
 }
 
 ?>
