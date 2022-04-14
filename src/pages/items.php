4c1644b4a12d15325565bd8c440f5e3d5a70b54d
loLLo32 <alessandro.lollato@barsanti.edu.it>
2022-04-14 22:56:03 +0200

    Item page changes

--- src/pages/items.php
+++ src/pages/items.php
@@ -37,21 +37,6 @@ function is_selected(string $order_value){
     <body>
         <?php echo(show_lateral_menu("Items")); ?>
         <div class="body_main">
-            <div class="items_research">
-                <form method="POST" action="" class="form_research_content">
-                    <input class="research_bar" type="text" name="content_filter" placeholder="Search..." autocomplete="off">
-                    <input class="research_btn" type="submit" value="Go">
-                </form>
-                <form method="POST" action="" class="form_content_order">
-                    <select class="content_order_selection" name="content_order">
-                        <option value="name" <?php if(is_selected("name")) { echo "selected";}?>>Name</option>
-                        <option value="category" <?php if(is_selected("category")) { echo "selected";}?>>Category</option>
-                        <option value="price" <?php if(is_selected("price")) { echo "selected";}?>>Price</option>
-                        <option value="brand" <?php if(is_selected("brand")) { echo "selected";}?>>Brand</option>
-                    </select>
-                    <input class="research_btn" type="submit" value="Order">
-                </form>
-            </div>
             <div class="items_list">
                 <?php echo(show_items($items)); ?>
             </div>
