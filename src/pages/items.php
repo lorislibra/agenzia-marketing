a4d8736effae069e09248b736f90660349242b83
loLLo32 <alessandro.lollato@barsanti.edu.it>
2022-04-14 10:34:22 +0200

    Items page changes

--- src/pages/items.php
+++ src/pages/items.php
@@ -1,5 +1,6 @@
 <?php
     require_once("src/template/lateral_menu.php");
+    require_once("src/template/items_template.php");
 ?>
 
 <html>
@@ -13,10 +14,11 @@
         <?php echo(show_lateral_menu("Items")); ?>
         <div class="body_main">
             <div class="items_head">
-
+                <!-- barra di ricerca -->
+                <!-- filtri -->
             </div>
             <div class="items_list">
-                <?php //echo(show_items()); ?>
+                <?php echo(show_items()); ?>
             </div>
         </div>
     </body>
