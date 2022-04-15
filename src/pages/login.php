a8d6e51b5ca23ecfd2ef1427213380fbe1ded7fa
loLLo32 <alessandro.lollato@barsanti.edu.it>
2022-04-15 11:56:04 +0200

    Login and logout

--- src/pages/login.php
+++ src/pages/login.php
@@ -35,11 +35,11 @@ if (is_post()) {
     </head>
     <body>
         <div class="login_section">
-            <img class="lm_title" src="https://peroni.it/wp-content/themes/birraperoni/assets/svg/peroni.svg">
-            <form method="POST" action="">
-                <input type="text" name="email" placeholder="Email" autocomplete="off">
-                <input type="password" name="password" placeholder="Password" autocomplete="off">
-                <input type="submit" value="LOGIN">
+            <img class="logo" src="https://peroni.it/wp-content/themes/birraperoni/assets/svg/peroni.svg">
+            <form class="login_form" method="POST" action="">
+                <input class="login_input" type="text" name="email" placeholder="Email" autocomplete="off">
+                <input class="login_input" type="password" name="password" placeholder="Password" autocomplete="off">
+                <input class="login_button" type="submit" value="LOGIN">
             </form>
         </div>
 
--- src/templates/lateral_menu.php
+++ src/templates/lateral_menu.php
@@ -6,16 +6,17 @@
 function show_lateral_menu(string $opened_section): string
 {
     $lm_links = array(
-        array("icon_url" => "https://img.icons8.com/ios-filled/344/ffffff/dog-house.png", "link_url" => "index.php", "link_text" => "Home"),
-        array("icon_url" => "https://img.icons8.com/wired/344/ffffff/circled-user.png", "link_url" => "login.php", "link_text" => "Account"),
-        array("icon_url" => "https://img.icons8.com/wired/344/ffffff/bulleted-list.png", "link_url" => "items.php", "link_text" => "Items"),
-        array("icon_url" => "https://img.icons8.com/ios-glyphs/344/ffffff/shopping-cart--v1.png", "link_url" => "login.php", "link_text" => "Cart")
+        array("icon_url" => "https://img.icons8.com/ios-filled/344/ffffff/dog-house.png", "link_url" => "index.php", "link_text" => "Home", "method" => "GET"),
+        array("icon_url" => "https://img.icons8.com/wired/344/ffffff/circled-user.png", "link_url" => "login.php", "link_text" => "Account", "method" => "GET"),
+        array("icon_url" => "https://img.icons8.com/wired/344/ffffff/bulleted-list.png", "link_url" => "items.php", "link_text" => "Items", "method" => "GET"),
+        array("icon_url" => "https://img.icons8.com/ios-glyphs/344/ffffff/shopping-cart--v1.png", "link_url" => "login.php", "link_text" => "Cart", "method" => "GET"),
+        array("icon_url" => "https://img.icons8.com/wired/344/ffffff/logout-rounded-left.png", "link_url" => "logout.php", "link_text" => "Log out", "method" => "POST")
     );
 
     $lm_html_links = '';
     foreach($lm_links as $lm_link){
         if($lm_link["link_text"] != $opened_section){
-            $lm_html_links .= add_link($lm_link["icon_url"], $lm_link["link_url"], $lm_link["link_text"]);
+            $lm_html_links .= add_link($lm_link["icon_url"], $lm_link["link_url"], $lm_link["link_text"], $lm_link["method"]);
         }
     }
 
@@ -43,7 +44,7 @@ function add_header(){
 }
 
 // Return the html code of a link in the lateral menu
-function add_link(string $icon_url, string $link_url, string $link_text): string
+function add_link(string $icon_url, string $link_url, string $link_text, string $link_method): string
 {
     $img_html_code = '
                     <img class="lm_link_img" src="' . $icon_url . '" alt="' . $link_text . '_icon">
@@ -51,7 +52,11 @@ function add_link(string $icon_url, string $link_url, string $link_text): string
     $link_text = '<p class="lm_link_text">' . $link_text . '</p>';
     $link_text = (!empty($icon_url)) ? $img_html_code . $link_text : $link_text;
     $html_code = '
-                <a class="lm_link" href="' . $link_url . '">' . $link_text . '</a>
+                <form class="lm_link" method="' . $link_method . '" action="' . $link_url .'">
+                    <button class="hidden_submit">
+                    ' . $link_text . '
+                    </button>
+                </form>
                 ';
 
     return $html_code;
