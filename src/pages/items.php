27158ae8f518c13db1dd0fe74ba70fb01ffd3d79
loLLo32 <alessandro.lollato@barsanti.edu.it>
2022-04-15 11:18:45 +0200

    Login

--- src/pages/items.php
+++ src/pages/items.php
@@ -12,14 +12,6 @@ $connection = DbManager::build_connection_from_env();
 $item_repo = new ItemRepo($connection);
 $items = $item_repo->get_all();
 
-function is_selected(string $order_value){
-    if(!empty($_POST["content_order"]) && $_POST["content_order"] == $order_value){
-        return true;
-    }
-
-    return false;
-}
-
 ?>
 
 <html>
